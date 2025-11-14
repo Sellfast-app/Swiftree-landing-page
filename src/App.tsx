064637a8bc4e31c30c97bea23edd0711e11cffa1
caleb <cakpomughe@gmail.com>
2025-11-14 15:15:45 +0100

    removed vercel rewrites

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Index from "./pages/Index";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 import AboutPage from "./pages/About";
+import StorefrontRedirect from "./pages/StorefrontRedirects";
 
 const queryClient = new QueryClient();
 
@@ -20,6 +21,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/login" element={<Login />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/storefront/*" element={<StorefrontRedirect />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
---
+++ src/pages/StorefrontRedirects.tsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+
+export default function StorefrontRedirect() {
+  useEffect(() => {
+    const path = window.location.pathname;
+    const trimmed = path.replace(/^\/storefront/, ""); // remove initial /storefront
+    const target = `https://storefront-jade-nu.vercel.app/storefront${trimmed}`;
+
+    window.location.href = target;
+  }, []);
+
+  return <div>Redirecting...</div>;
+}
